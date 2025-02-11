2d7d7f111247c6e3a34393a3a2ac7b6fe4891f59
Jason Davidson <jad658@gmail.com>
2025-02-10 19:40:46 -0800

    Remove window boarders

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,8 @@
     screensaver.askForPassword = true;
 
     trackpad.TrackpadRightClick = true;
+
+    WindowManager.EnableTiledWindowMargins = false;
   };
 
   launchd.user.agents.no-finder = {
